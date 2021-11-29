dc2b61ae7e231cc6b950a7c919e5c2ce66b617e6
Fran Camaj <fran.camaj@limitmarkets.com>
2021-11-29 19:20:18 +0100

    Install swagger, config and some other necessary dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('online-library')
+    .setDescription('The online-library API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  config.security = [{ bearer: [] }];
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
